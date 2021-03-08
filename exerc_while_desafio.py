73c55fa9af1ad4383e7eb83d08d2c98ca81c1b09
Fil Tuon <f.tuon887@gmail.com>
2021-03-07 21:38:30 -0300

    .

--- exerc_while_desafio.py
+++ exerc_while_desafio.py
@@ -1,2 +1,20 @@
-# O Computador ira pensa em um numero entre 0 e 10.
+# O Computador ira pensar em um numero entre 0 e 10.
 #So que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessarios para venceer.
+from random import randint
+
+computador = randint(0, 10)
+print('Sou seu computador e acabei de pensar num nunmero entre 0 e 10')
+print('Será que você consegue adivinhar?')
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('Qual é seu palpite? '))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Mais...Tente mais uma vez')
+        elif jogador > computador:
+            print('Menos...Tente mais uma vez')
+print('Acertou com {} tentativas. Parabéns'.format(palpites))
--- exerc_while_sexo.py
+++ exerc_while_sexo.py
@@ -1,2 +1,8 @@
 #Faça um programa que leia o sexo de uma pessoa. mas só aceite os valores M ou F
 #Caso esteja errado, peça a digitação novamente até ter um valor correto.
+
+sexo = str(input('Escolha seu sexo [M ou F]: ')).strip().upper()[0] #pega somente a primeira letra
+idade = int(input('Digite sua idade'))
+while sexo  not in 'MmFf':
+    sexo = str(input('Dados invalidos. Por favor informe seu sexo novamente: ')).strip().upper()[0]
+print('Sexo registrado com sucesso'.format(sexo))
