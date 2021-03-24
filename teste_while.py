ace6377c8582e64269b2943f4560d11938590bfe
Fil Tuon <f.tuon887@gmail.com>
2021-03-24 16:04:06 -0300

    .

---
+++ teste_while.py
@@ -0,0 +1,4 @@
+'''Se usarmos a estrutura for c in range(0, 10, 3): no Python e colocarmos dentro do bloco um comando print(c), quais serão os valores impressos na tela do terminal?'''
+
+for c in range (0, 5):
+    print(c)
